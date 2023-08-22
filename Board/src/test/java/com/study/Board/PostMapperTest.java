d6eb02533256b748407d4a1695be8267b168ad33
fullm <matoma1_@naver.com>
2023-08-22 15:39:33 +0900

    updatetest
     게시글을 수정하기 위해 수정할 게시글의  pk인  id를 파라미터로 전달해줘야함

--- Board/src/test/java/com/study/Board/PostMapperTest.java
+++ Board/src/test/java/com/study/Board/PostMapperTest.java
@@ -41,4 +41,27 @@ public class PostMapperTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    void update(){
+        //1. 게시글 수정
+         PostRequest params = new PostRequest();
+        params.setId(1L);
+        params.setTitle("1번 게시글 제목 수정합니다.");
+        params.setContent("1번 게시글 내용 수정합니다.");
+        params.setWriter("도뎡이");
+        params.setNoticeYn(true);
+        postMapper.update(params);
+
+        // 2. 게시글 상세정보 조회
+        PostResponse post = postMapper.findById(1L);
+        try {
+            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+            System.out.println(postJson);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
