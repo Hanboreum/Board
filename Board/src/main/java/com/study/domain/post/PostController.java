c5d7000850215f7c977f6686cad7ef1fc0db883f
fullm <matoma1_@naver.com>
2023-08-25 14:09:41 +0900

    컨트롤러에메서드추가

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -1,5 +1,6 @@
 package com.study.domain.post;
 
+import com.study.common.dto.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -57,5 +58,10 @@ public class PostController {
     public String deletePost(@RequestParam final Long id) {
         postService.deletePost(id);
         return "redirect:/post/list.do";
+    }
+    // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) {
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
     }
 }
