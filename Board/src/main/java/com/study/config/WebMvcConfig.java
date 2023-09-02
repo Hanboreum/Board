c17554476e014126b5599d3b4c3d3c82323dc115
fullm <matoma1_@naver.com>
2023-09-02 20:55:54 +0900

    애플리케이션인터셉터 등록하기

--- Board/src/main/java/com/study/config/WebMvcConfig.java
+++ Board/src/main/java/com/study/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.study.config;
 
 import com.study.interceptor.LoggerInterceptor;
+import com.study.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
                 .excludePathPatterns("/css/**", "/images/**", "/js/**");
 
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**/*.do")
+                .excludePathPatterns("/log*");
     }
+
 }
