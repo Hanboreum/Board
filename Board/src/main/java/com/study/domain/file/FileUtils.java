1593c5a41ec981b7f5a27b6a0118b8b290d89b5b
fullm <matoma1_@naver.com>
2023-10-26 17:31:51 +0900

    version1.1.0 파일 다운로드

--- Board/src/main/java/com/study/domain/file/FileUtils.java
+++ Board/src/main/java/com/study/domain/file/FileUtils.java
@@ -1,5 +1,7 @@
 package com.study.domain.file;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -7,6 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -114,6 +118,7 @@ public class FileUtils {
 
     /**
      * 파일 삭제 (from Disk)
+     *
      * @param files - 삭제할 파일 정보 List
      */
     public void deleteFiles(final List<FileResponse> files) {
@@ -128,7 +133,8 @@ public class FileUtils {
 
     /**
      * 파일 삭제 (from Disk)
-     * @param addPath - 추가 경로
+     *
+     * @param addPath  - 추가 경로
      * @param filename - 파일명
      */
     private void deleteFile(final String addPath, final String filename) {
@@ -138,6 +144,7 @@ public class FileUtils {
 
     /**
      * 파일 삭제 (from Disk)
+     *
      * @param filePath - 파일 경로
      */
     private void deleteFile(final String filePath) {
@@ -147,5 +154,19 @@ public class FileUtils {
         }
     }
 
+    public Resource readFileAsResource(final FileResponse file) {
+        String uploadedDate = file.getCreatedDate().toLocalDate().format(DateTimeFormatter.ofPattern("yyMMdd"));
+        String filename = file.getSaveName();
+        Path filePath = Paths.get(uploadPath, uploadedDate, filename);
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists() == false || resource.isFile() == false) {
+                throw new RuntimeException("file not found : " + filePath.toString());
+            }
+            return resource;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("file not found : " + filePath.toString());
+        }
 
+    }
 }
